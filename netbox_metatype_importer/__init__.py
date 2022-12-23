09c17a2b8ed10005210df7a78bb5a0356b4060e4
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-12-24 00:34:26 +0530

    Update netbox_metatype_importer/__init__.py

--- netbox_metatype_importer/__init__.py
+++ netbox_metatype_importer/__init__.py
@@ -2,7 +2,7 @@ from extras.plugins import PluginConfig
 from .version import __version__
 
 
-class NetboxmetatypeimporterConfig(PluginConfig):
+class NetBoxMetatypeImporterConfig(PluginConfig):
     name = 'netbox_metatype_importer'
     verbose_name = 'MetaType Importer'
     description = 'Import MetaType from github repo'
