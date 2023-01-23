c223ebd57c278154116921a7f4572b5823350b9b
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-01-24 01:29:18 +0530

    fixed home page link

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Import Metatypes',
     long_description='Import MetaTypes into NetBox',
     long_description_content_type="text/markdown",
-    url='https://github.com/k01ek/netbox-metatype-importer',
+    url='https://github.com/Onemind-Services-LLC/netbox-metatype-importer',
     author='Abhimanyu Saharan',
     author_email='asaharan@onemindservices.com',
     maintainer='Prince Kumar',
