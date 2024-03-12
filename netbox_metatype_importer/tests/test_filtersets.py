d7c718b9e256b54d1cdd4fa4f3d46fd93ade7561
yash-pal1 <ypal@onemindservies.com>
2024-03-12 11:33:05 +0530

    lint fix #39

--- netbox_metatype_importer/tests/test_filtersets.py
+++ netbox_metatype_importer/tests/test_filtersets.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from ..filters import *
 from ..models import *
 
+
 class MetatypeImporterTests(TestCase):
     queryset = MetaType.objects.all()
     filterset = MetaTypeFilterSet
